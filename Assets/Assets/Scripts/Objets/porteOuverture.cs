34ebcdedc30c854f12b9c9ed3ff2790f776b3ec5
Emile Litalien <144373958+CeciEstUnPseudo@users.noreply.github.com>
2025-04-06 23:16:43 -0400

    Re-link avec notre projet en cours
    L'intégrateur a perdu son lien entre l'ordinateur et le github. Voici un copier-coller du package de travail (up-to-date)

---
+++ Assets/Assets/Scripts/Objets/porteOuverture.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class porteOuverture : MonoBehaviour
+{
+    private bool proximiteJoueur;
+    private bool canGoOffline = false;
+
+    private bool porteDisponible = true;
+    private bool porteStatus = false;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        proximiteJoueur = gameObject.GetComponent<objetProximity>().actif;
+
+
+
+
+
+        if (proximiteJoueur)
+        {
+            // Porte ferme
+            if (porteDisponible && !porteStatus)
+            {
+                Debug.Log("Porte ouvre");
+                Invoke("ouvrirPorte", 0f);
+            }
+
+        } else if (canGoOffline)
+        {
+            // Porte ferme
+            if (porteDisponible && porteStatus)
+            {
+                Debug.Log("Porte ouvre");
+                Invoke("fermerPorte", 0f);
+            }
+        }
+    }
+
+    public void ouvrirPorte()
+    {
+
+        Debug.Log("Invoke Porte ouvre");
+
+        porteDisponible = false;
+        Invoke("rendrePorteDisponible", 1f);
+
+
+        // mettre animation ouverture
+        this.GetComponent<Animator>().SetTrigger("OuvrirDoor");
+
+        canGoOffline = true;
+    }
+
+    public void fermerPorte()
+    {
+        Debug.Log("Invoke Porte ouvre");
+
+        porteDisponible = false;
+        Invoke("rendrePorteDisponible", 1f);
+
+
+        // mettre animation fermeture
+        this.GetComponent<Animator>().SetTrigger("FermerDoor");
+
+        canGoOffline = false;
+    }
+
+    public void rendrePorteDisponible()
+    {
+
+        Debug.Log("Rend Porte Disponible");
+
+        porteDisponible = true;
+        porteStatus = !porteStatus;
+    }
+}
