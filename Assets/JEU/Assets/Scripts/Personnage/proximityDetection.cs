93c16ad5080226f5f37769740828bb7387d38132
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2025-04-14 08:23:42 -0400

    Combat + Ennemis
    - Ennemis Roaming
    - Ennemis qui prennent du dommage (quand le joueur attaque)
    - Attaque se débloque quand on prend l'upgrade
    - Fix un bug avec multiples hitbox de l'attaque

--- Assets/JEU/Assets/Scripts/Personnage/proximityDetection.cs
+++ Assets/JEU/Assets/Scripts/Personnage/proximityDetection.cs
@@ -4,23 +4,27 @@ public class proximityDetection : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    // Variable pour montrer au dev que le joueur est proche d'un objet et peut intéragir
     [SerializeField] private bool canInteract = false;
-
-    void Start()
-    {
-    }
-
 
 
     private void OnTriggerEnter(Collider other)
     {
-
-       if (other.gameObject.tag == "interactable")
-       {
+        // Si il entre dans le cercle d'interaction (sphere collider Trigger ON) d'un objet
+        if (other.gameObject.tag == "interactable")
+        {
             other.GetComponent<objetProximity>().actif = true;
 
-            
+
             canInteract = true;
+        }
+
+        // Si il entre dans le cercle d'interaction (sphere collider Trigger ON) d'un ennemi
+        if (other.gameObject.tag == "ennemi")
+        {
+            // Joueur se fait pousser vers l'arriere (on essai d'attaquer le joueur)
+            other.GetComponent<ennemiComportement>().atteintJoueur();
+
         }
     }
 
@@ -32,5 +36,7 @@ public class proximityDetection : MonoBehaviour
 
             canInteract = false;
         }
+
+
     }
 }
