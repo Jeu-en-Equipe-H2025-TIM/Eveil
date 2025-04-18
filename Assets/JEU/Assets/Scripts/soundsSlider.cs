9f2a2ac8f9ae6b19ec704e62cfa839f6c53abdbc
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2025-04-18 16:29:15 -0400

    Intégration barre de son fonctionnelle
    Marche en dev-prog. À intégrer

---
+++ Assets/JEU/Assets/Scripts/soundsSlider.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class soundsSlider : MonoBehaviour
+{
+
+    [SerializeField] Slider slider;
+    [SerializeField] AudioMixer mixerPrincipal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        setVolume(PlayerPrefs.GetFloat("volumeSauvegarder", 100));
+    }
+
+    public void setVolume(float valeur)
+    {
+        if (valeur < 1)
+        {
+            valeur = 0.001f;
+        }
+        updateSlider(valeur);
+        mixerPrincipal.SetFloat("MasterVolume", Mathf.Log10(valeur / 100) * 20f);
+        
+    }
+
+    public void SetVolumeDuSlider()
+    {
+        setVolume(slider.value);
+    }
+
+    public void updateSlider(float valeur)
+    {
+        slider.value = valeur;
+    }
+}
